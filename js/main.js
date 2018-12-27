09a45a3c1e29f9dc2de4db12e71b4183618eaaaa
Pavlo Grechka <pavlogrechka@gmail.com>
2018-12-27 21:07:42 +0200

    js file

---
+++ js/main.js
@@ -0,0 +1,46 @@
+'use strict';
+
+// // var number = 5;
+// // var string = "Hello!";
+// // var sym = Symbol();
+// // var boolean = true;
+// // null;
+// // undefined;
+// // var obj = {};
+
+// // console.log(4/0);
+// // console.log('string'*9);
+// // let something =1;
+// // console.log(something);
+
+// // let persone = {
+// //     name: "John",
+// //     age: 25,
+// //     isMarried: false
+// // };
+// // console.log(persone.name);
+// // console.log(persone["age"]);
+
+// let arr = ['plum.png','orange.jpg','apple.bmp'];
+// // console.log(arr[0]);
+
+// // alert("Hello, World!");
+
+// let answer = +prompt("how old are you? have you 18?");
+// console.log(typeof(answer));
+// console.log("arr" + " - object");
+// console.log(4 + " - object");
+
+let incr = 10,
+    decr = 10;
+
+// incr++
+// // decr--
+// console.log(incr++);
+// console.log(decr--);
+// console.log(11%2);
+// console.log("2" === 2)
+let isChecked = false,
+    isClose = false;
+console.log(isChecked || !isClose);
+
